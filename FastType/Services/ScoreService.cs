d84c745df5f9f2fbf5580513bceab6c89e38e5a1
Kauã Vinícius <kauaponte@hotmail.com>
2024-11-08 16:17:15 -0300

    Funny part =)

--- FastType/Services/ScoreService.cs
+++ FastType/Services/ScoreService.cs
@@ -4,8 +4,27 @@ public static class ScoreService
 {
     private static int GapPenalty { get; set; } = 3;
     private static int MMPenalty { get; set; } = 1;
-    public static double Match(string FWord, string SWord)
+    public static double Match(string SWord, string WWord)
     {
-        return 0.0;
+        List<List<int>> Memo = [];
+        for (int i = 0; i < SWord.Length; i++)
+            Memo[0][i] = GapPenalty * i;
+
+        for (int i = 0; i < WWord.Length; i++)
+            Memo[i][0] = GapPenalty * i;
+
+        for (int i = 0; i < SWord.Length; i++)
+        {
+            for (int j = 0; j < WWord.Length; j++)
+            {
+                int penalty = 0;
+                if (SWord[i] != WWord[j])
+                    penalty = MMPenalty;
+                Memo[i][j] = Math.Min(penalty + Memo[i - 1][j - 1],
+                             Math.Min(GapPenalty + Memo[i - 1][j],
+                                      GapPenalty + Memo[i][j - 1]));
+            }
+        }
+        return Memo[SWord.Length][WWord.Length];
     }
 }
