13ff879c904d90936ada57dd5df8cbf6d4ae8d7f
Kauã Vinícius <kauaponte@hotmail.com>
2024-11-12 12:34:37 -0300

    Funny part

--- FastType/Services/ScoreService.cs
+++ FastType/Services/ScoreService.cs
@@ -6,16 +6,22 @@ public static class ScoreService
     private static int MMPenalty { get; set; } = 1;
     public static double Match(string SWord, string WWord)
     {
-        List<List<int>> Memo = [];
+        double max = Math.Max(SWord.Length * GapPenalty, WWord.Length * GapPenalty);
+
+
+        int[][] Memo = new int[SWord.Length][];
         for (int i = 0; i < SWord.Length; i++)
-            Memo[0][i] = GapPenalty * i;
+            Memo[i] = new int[WWord.Length];
 
         for (int i = 0; i < WWord.Length; i++)
-            Memo[i][0] = GapPenalty * i;
+            Memo[0][i] = GapPenalty * i;
 
         for (int i = 0; i < SWord.Length; i++)
+            Memo[i][0] = GapPenalty * i;
+
+        for (int i = 1; i < SWord.Length; i++)
         {
-            for (int j = 0; j < WWord.Length; j++)
+            for (int j = 1; j < WWord.Length; j++)
             {
                 int penalty = 0;
                 if (SWord[i] != WWord[j])
@@ -23,8 +29,10 @@ public static class ScoreService
                 Memo[i][j] = Math.Min(penalty + Memo[i - 1][j - 1],
                              Math.Min(GapPenalty + Memo[i - 1][j],
                                       GapPenalty + Memo[i][j - 1]));
+                max = Math.Max(max, Memo[i][j]);
             }
         }
-        return Memo[SWord.Length][WWord.Length];
+        var limiter = (SWord.Length * Math.Min(GapPenalty, MMPenalty)) / 2;
+        return limiter - Memo[SWord.Length - 1][WWord.Length - 1];
     }
 }
